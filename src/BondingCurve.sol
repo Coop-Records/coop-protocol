1b1274efe2cc68d1361620d058ead2b150228986
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-22 12:12:59 +0700

    I add required contracts for WowFactoryDEployment.

---
+++ src/BondingCurve.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
+
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkkkkkkO0KXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNOdl:'...        ...':lx0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0d:.                        .:d0NMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWKd;.                              .;dKWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMW0l.                                    .l0WMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMNKOkkxkO0KOl.                                        .lKWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMW0l'.       ..                                            'xNMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMO'   .',,,.                                                .lXMMMMMMMMMMMMMMMMMM
+MMMMMMMMMWo   ,0NWWK:                                                  cXMMMMMMMMMMMMMMMMM
+MMMMMMMMMMk.  .xWMNo                      .',,,,'.                      oNMMMMMMMMMMMMMMMM
+MMMMMMMMMMNo.  .oXx.                  .,oOKNWWWWNKOo,                   .kWMMMMMMMMMMMMMMM
+MMMMMMMMMMMNx.   '.                  ,kNMMMMMMMMMMMMNk,                  :XMMMMMMMMMMMMMMM
+MMMMMMMMMMMMW0:.                    cXMMMMMMMMMMMMMMMMK:                 .kMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNk;                  ,0MMMMMMXkddkNMMMMMM0'                 dWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNk;                cNMMMMMNo   .oWMMMMMX:                 oWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMNO:.             ;XMMMMMW0c;;l0WMMMMMK,                 oWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMW0l'           .dWMMMMMMMWWMMMMMMMNo.                .kMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNO0WMMXx:.         .oXMMMMMMMMMMMMMMXl.                 ,KMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWd';xXWMWKd;.        'o0NMMMMMMMWNOo'                  .dWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMX:  .lONMMW0o,.       .':looool:'.                    .OWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMM0;    ,o0NMMN0d;.                                     'dNMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMM0;     .,o0NMMWKxc.                               ..   :0WMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMXl.      .,lONWMWXOo;.                         .lK0;   'OWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNk,         'cxKWMMWKkl:;.                   ,kWMMXl   ,0MMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMXd'          .,lkXWMMMWKko;..            .oXMMMMM0,  .dWMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMXx;.           .;ok0XWMMWX0xl;..       .,;cclcc,   .kMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWKd;.            ..;lx0NWMMWX0koc;'..          .;kWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXko:,..           .'cxKWMMMMMWNK0OxdolllodkKWMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNX0kxolllccllooxk0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+contract BondingCurve {
+    using FixedPointMathLib for uint256;
+    using FixedPointMathLib for int256;
+
+    error InsufficientLiquidity();
+
+    // y = A*e^(Bx)
+    uint256 public immutable A = 1060848709;
+    uint256 public immutable B = 4379701787;
+
+    function getEthSellQuote(
+        uint256 currentSupply,
+        uint256 ethOrderSize
+    ) external pure returns (uint256) {
+        uint256 deltaY = ethOrderSize;
+        uint256 x0 = currentSupply;
+        uint256 exp_b_x0 = uint256((int256(B.mulWad(x0))).expWad());
+
+        uint256 exp_b_x1 = exp_b_x0 - deltaY.fullMulDiv(B, A);
+        uint256 x1 = uint256(int256(exp_b_x1).lnWad()).divWad(B);
+        uint256 tokensToSell = x0 - x1;
+
+        return tokensToSell;
+    }
+
+    function getTokenSellQuote(
+        uint256 currentSupply,
+        uint256 tokensToSell
+    ) external pure returns (uint256) {
+        if (currentSupply < tokensToSell) revert InsufficientLiquidity();
+        uint256 x0 = currentSupply;
+        uint256 x1 = x0 - tokensToSell;
+
+        uint256 exp_b_x0 = uint256((int256(B.mulWad(x0))).expWad());
+        uint256 exp_b_x1 = uint256((int256(B.mulWad(x1))).expWad());
+
+        // calculate deltaY = (a/b)*(exp(b*x0) - exp(b*x1))
+        uint256 deltaY = (exp_b_x0 - exp_b_x1).fullMulDiv(A, B);
+
+        return deltaY;
+    }
+
+    function getEthBuyQuote(
+        uint256 currentSupply,
+        uint256 ethOrderSize
+    ) external pure returns (uint256) {
+        uint256 x0 = currentSupply;
+        uint256 deltaY = ethOrderSize;
+
+        // calculate exp(b*x0)
+        uint256 exp_b_x0 = uint256((int256(B.mulWad(x0))).expWad());
+
+        // calculate exp(b*x0) + (dy*b/a)
+        uint256 exp_b_x1 = exp_b_x0 + deltaY.fullMulDiv(B, A);
+
+        uint256 deltaX = uint256(int256(exp_b_x1).lnWad()).divWad(B) - x0;
+
+        return deltaX;
+    }
+
+    function getTokenBuyQuote(
+        uint256 currentSupply,
+        uint256 tokenOrderSize
+    ) external pure returns (uint256) {
+        uint256 x0 = currentSupply;
+        uint256 x1 = tokenOrderSize + currentSupply;
+
+        uint256 exp_b_x0 = uint256((int256(B.mulWad(x0))).expWad());
+        uint256 exp_b_x1 = uint256((int256(B.mulWad(x1))).expWad());
+
+        uint256 deltaY = (exp_b_x1 - exp_b_x0).fullMulDiv(A, B);
+
+        return deltaY;
+    }
+}
---
+++ src/interfaces/IWowFactory.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkkkkkkO0KXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNOdl:'...        ...':lx0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0d:.                        .:d0NMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWKd;.                              .;dKWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMW0l.                                    .l0WMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMNKOkkxkO0KOl.                                        .lKWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMW0l'.       ..                                            'xNMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMO'   .',,,.                                                .lXMMMMMMMMMMMMMMMMMM
+MMMMMMMMMWo   ,0NWWK:                                                  cXMMMMMMMMMMMMMMMMM
+MMMMMMMMMMk.  .xWMNo                      .',,,,'.                      oNMMMMMMMMMMMMMMMM
+MMMMMMMMMMNo.  .oXx.                  .,oOKNWWWWNKOo,                   .kWMMMMMMMMMMMMMMM
+MMMMMMMMMMMNx.   '.                  ,kNMMMMMMMMMMMMNk,                  :XMMMMMMMMMMMMMMM
+MMMMMMMMMMMMW0:.                    cXMMMMMMMMMMMMMMMMK:                 .kMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNk;                  ,0MMMMMMXkddkNMMMMMM0'                 dWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNk;                cNMMMMMNo   .oWMMMMMX:                 oWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMNO:.             ;XMMMMMW0c;;l0WMMMMMK,                 oWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMW0l'           .dWMMMMMMMWWMMMMMMMNo.                .kMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNO0WMMXx:.         .oXMMMMMMMMMMMMMMXl.                 ,KMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWd';xXWMWKd;.        'o0NMMMMMMMWNOo'                  .dWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMX:  .lONMMW0o,.       .':looool:'.                    .OWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMM0;    ,o0NMMN0d;.                                     'dNMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMM0;     .,o0NMMWKxc.                               ..   :0WMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMXl.      .,lONWMWXOo;.                         .lK0;   'OWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNk,         'cxKWMMWKkl:;.                   ,kWMMXl   ,0MMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMXd'          .,lkXWMMMWKko;..            .oXMMMMM0,  .dWMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMXx;.           .;ok0XWMMWX0xl;..       .,;cclcc,   .kMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWKd;.            ..;lx0NWMMWX0koc;'..          .;kWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXko:,..           .'cxKWMMMMMWNK0OxdolllodkKWMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNX0kxolllccllooxk0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+interface IWowFactory {
+    /// @notice Emitted when a new Wow token is created
+    /// @param factoryAddress The address of the factory that created the token
+    /// @param tokenCreator The address of the creator of the token
+    /// @param platformReferrer The address of the platform referrer
+    /// @param protocolFeeRecipient The address of the protocol fee recipient
+    /// @param bondingCurve The address of the bonding curve
+    /// @param tokenURI The URI of the token
+    /// @param name The name of the token
+    /// @param symbol The symbol of the token
+    /// @param tokenAddress The address of the token
+    /// @param poolAddress The address of the pool
+    event WowTokenCreated(
+        address indexed factoryAddress,
+        address indexed tokenCreator,
+        address platformReferrer,
+        address protocolFeeRecipient,
+        address bondingCurve,
+        string tokenURI,
+        string name,
+        string symbol,
+        address tokenAddress,
+        address poolAddress
+    );
+
+    /// @notice Deploys a Wow ERC20 token
+    /// @param _tokenCreator The address of the token creator
+    /// @param _platformReferrer The address of the platform referrer
+    /// @param _tokenURI The ERC20z token URI
+    /// @param _name The ERC20 token name
+    /// @param _symbol The ERC20 token symbol
+    function deploy(
+        address _tokenCreator,
+        address _platformReferrer,
+        string memory _tokenURI,
+        string memory _name,
+        string memory _symbol
+    ) external payable returns (address);
+}
